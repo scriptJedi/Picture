e099650361aa6c6b375899143dc779c50f7a0368
scriptJedi <sscriptjedi@gmail.com>
2023-12-17 12:29:50 +0200

    Add accordion script

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import showMoreStyles from "./modules/showMoreStyles";
 import calc from "./modules/calc";
 import filter from "./modules/filter";
 import pictureSize from "./modules/pictureSize";
+import accordion from "./modules/accordion";
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
@@ -19,4 +20,5 @@ window.addEventListener('DOMContentLoaded', () => {
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
     pictureSize('.sizes-block');
+    accordion('.accordion-heading');
 })
