97d62174332cce0a683a2caed9bd88f5d5fa4e36
scriptJedi <sscriptjedi@gmail.com>
2024-01-09 15:15:24 +0200

    add drag & drop functional

--- src/js/modules/drop.js
+++ src/js/modules/drop.js
@@ -1,5 +1,64 @@
 const drop = () => {
+    // drag *
+    // dragend *
+    // dragenter - объект над dropArea
+    // dragexit *
+    // dragleave - объект за пределами dropArea
+    // dragover - объект зависает над dropArea
+    // dragstart *
+    // drop - объект отправлен в dropArea
 
+    const fileInputs = document.querySelectorAll('[name="upload"]');
+
+    ['dragenter', 'dragleave', 'dragover', 'drop'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, preventDefaults, false);
+        })
+    })
+
+    function preventDefaults(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    function highLight(item) {
+        item.closest('.file_upload').style.border = "5px solid yellow";
+        item.closest('.file_upload').style.backgroundColor = "rgba(0,0,0,.7)";
+    }
+
+    function unHighLight(item) {
+        item.closest('.file_upload').style.border = "none";
+        if (item.closest('.calc-form')) {
+            item.closest('.file_upload').style.backgroundColor = "#fff";
+        } else {
+            item.closest('.file_upload').style.backgroundColor = "#ededed";
+        }
+    }
+
+    ['dragenter', 'dragover'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, () => highLight(input), false);
+        })
+    });
+
+    ['drop', 'dragleave'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, () => unHighLight(input), false);
+        })
+    })
+
+    fileInputs.forEach(input => {
+        input.addEventListener('drop', (e) => {
+            e.preventDefault();
+            input.files = e.dataTransfer.files;
+
+            let dots;
+            const arr = input.files[0].name.split('.');
+
+            arr[0].length > 6 ? dots = "..." : dots = '.';
+            input.previousElementSibling.textContent = arr[0].substring(0, 6) + dots + arr[1];
+        })
+    })
 }
 
 export default drop;
